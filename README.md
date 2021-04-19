534ec7fa21731cdb3cda5ff19ca1e45de58b9d12
ashwin <ashwin.magalu@gmail.com>
2021-04-19 17:09:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-tailwind-gallery-app
+ ReactJS | TailwindCSS | Pixabay
---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
+
+function App() {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setImages(data.hits);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+
+    return () => {
+      //
+    };
+  }, [term]);
+
+  return (
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!loading && images.length === 0 && (
+        <h1 className="text-6xl text-center mx-auto mt-32">No images found</h1>
+      )}
+
+      {loading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
